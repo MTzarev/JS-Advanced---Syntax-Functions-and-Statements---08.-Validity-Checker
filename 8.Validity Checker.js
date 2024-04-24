3a8b66a25a5b6073673a1eab69e8f8c7253a32bc
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-24 22:18:09 +0300

    Update 8.Validity Checker.js

--- 8.Validity Checker.js
+++ 8.Validity Checker.js
@@ -8,8 +8,6 @@ function isValidDistance(x1, y1, x2, y2) {
     } else {
         console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
     }
-
- 
     if (Number.isInteger(distance(x2, y2, 0, 0))) {
         console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
     } else {
